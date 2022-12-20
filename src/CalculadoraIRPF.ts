806a1b21b5732fe35763e1f437635d6f15276e68
Thiago Paiva <thiagohdaqw@gmail.com>
2022-12-19 22:22:07 -0300

    Duplicação: Cadastro de rendimento

--- src/CalculadoraIRPF.ts
+++ src/CalculadoraIRPF.ts
@@ -1,21 +1,31 @@
 
 
 export default class CalculadoraIRPF {
-    rendimento: string;
-    valor: number;
+    nomeRendimentos: string[];
+    valorRendimentos: number[];
+
+    constructor() {
+        this.valorRendimentos = [];
+        this.nomeRendimentos = [];
+    }
 
     cadastraRedimento(rendimento: string, valor: number) {
-        this.rendimento = rendimento;
-        this.valor = valor;
+        this.nomeRendimentos.push(rendimento);
+        this.valorRendimentos.push(valor);
     }
 
     getTotalRendimentos(): number {
-        return 500;
+        return this.valorRendimentos[0] + (this.valorRendimentos[1] ?? 0);
     }
 
-    getRendimentos(): [[string, number]] {
+    getRendimentos(): [string, number][] {
+        if (this.nomeRendimentos.length === 1) {
+            return [[this.nomeRendimentos[0], this.valorRendimentos[0]]]
+        }
+
         return [
-            ['Salario', 500.0]
-        ]
+            [this.nomeRendimentos[0], this.valorRendimentos[0]],
+            [this.nomeRendimentos[1], this.valorRendimentos[1]]
+        ];
     }
 }
--- src/__tests__/CalculadoraIRPF.test.ts
+++ src/__tests__/CalculadoraIRPF.test.ts
@@ -13,3 +13,17 @@ test('Cadastra um rendimento do tipo Salario', () => {
 });
 
 
+test('Cadastra mais um rendimento', () => {
+  const calculadora = new CalculadoraIRPF();
+
+  calculadora.cadastraRedimento('Salario', 500.0);
+  calculadora.cadastraRedimento('Aluguel', 750.50);
+
+  expect(calculadora.getTotalRendimentos())
+    .toBeCloseTo(1250.50, 2);
+  expect(calculadora.getRendimentos())
+    .toEqual([
+      ['Salario', 500.0],
+      ['Aluguel', 750.50]
+    ])
+});
