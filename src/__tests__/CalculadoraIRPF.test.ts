99c473c7e5fac295e53e78a76c0a531056e8a8ab
Thiago Paiva <thiagohdaqw@gmail.com>
2022-12-20 18:44:22 -0300

    Removendo testes nao parametrizados

--- src/__tests__/CalculadoraIRPF.test.ts
+++ src/__tests__/CalculadoraIRPF.test.ts
@@ -5,52 +5,6 @@ import ValorDeducaoInvalidoException from '../excecoes/ValorDeducaoInvalidoExcep
 
 describe('CalculadoraIRPF', () => {
 
-  it('cadastra um rendimento do tipo Salario', () => {
-    const calculadora = new CalculadoraIRPF();
-  
-    calculadora.cadastraRedimento('Salario', 500.0);
-  
-    expect(calculadora.getTotalRendimentos())
-      .toBeCloseTo(500.0, 2);
-    expect(calculadora.getRendimentos())
-      .toEqual([['Salario', 500.0]]);
-  });
-
-
-  it('cadastra dois rendimentos', () => {
-    const calculadora = new CalculadoraIRPF();
-  
-    calculadora.cadastraRedimento('Salario', 500.0);
-    calculadora.cadastraRedimento('Aluguel', 750.50);
-  
-    expect(calculadora.getTotalRendimentos())
-      .toBeCloseTo(1250.50, 2);
-    expect(calculadora.getRendimentos())
-      .toEqual([
-        ['Salario', 500.0],
-        ['Aluguel', 750.50]
-      ]);
-  });
-  
-  
-  it('Cadastra três rendimentos', () => {
-    const calculadora = new CalculadoraIRPF();
-  
-    calculadora.cadastraRedimento('Salario', 500.0);
-    calculadora.cadastraRedimento('Aluguel', 750.50);
-    calculadora.cadastraRedimento('Ação', 1550.50);
-  
-    expect(calculadora.getTotalRendimentos())
-      .toBeCloseTo(2801.0, 2);
-    expect(calculadora.getRendimentos())
-      .toEqual([
-        ['Salario', 500.0],
-        ['Aluguel', 750.50],
-        ['Ação', 1550.50]
-      ])
-  });
-
-
   it.each<[[string, number][], number]>([
       [[['Salario', 100], ['Aluguel', 500], ['Ação', 800.5], ['Dividendos', 800.1]], 2200.6],
       [[['Lucros', 987.64], ['Aluguel', 0.01]], 987.65],
