c1b1eab769005b16e27e554fd127c24ff42b1b2d
João Pedro Chaves <joaopedroaschaves@gmail.com>
2022-12-21 19:15:32 -0300

    Remoção de testes não parametrizados de cadastro de pensão

--- src/__tests__/CalculadoraIRPF.test.ts
+++ src/__tests__/CalculadoraIRPF.test.ts
@@ -120,48 +120,6 @@ describe('CalculadoraIRPF', () => {
     }).toThrowError(ValorDeducaoInvalidoException);
   });
 
-  test('Cadastra contribuição previdenciária oficial no contracheque', () => {
-    const calculadora = new CalculadoraIRPF();
-
-    calculadora.cadastraContribuicaoPrevidenciaria('Contribuicao no contracheque', 100.0);
-
-    expect(calculadora.getTotalContribuicaoPrevidenciaria())
-      .toBeCloseTo(100.0, 2);
-    expect(calculadora.getContribuicaoPrevidenciaria())
-      .toEqual([['Contribuicao no contracheque', 100.0]]);
-  });
-
-  test('Cadastra segunda contribuição previdenciária oficial no contracheque', () => {
-    const calculadora = new CalculadoraIRPF();
-
-    calculadora.cadastraContribuicaoPrevidenciaria('Contribuicao no contracheque', 100.0);
-    calculadora.cadastraContribuicaoPrevidenciaria('Contribuição via carnê INSS', 130.0);
-
-    expect(calculadora.getTotalContribuicaoPrevidenciaria())
-      .toBeCloseTo(230.0, 2);
-    expect(calculadora.getContribuicaoPrevidenciaria())
-      .toEqual([
-        ['Contribuicao no contracheque', 100.0],
-        ['Contribuição via carnê INSS', 130.0]
-      ]);
-  });
-
-  test('Cadastra terceira contribuicao previdenciaria', () => {
-    const calculadora = new CalculadoraIRPF();
-
-    calculadora.cadastraContribuicaoPrevidenciaria('Contribuicao no contracheque', 100.0);
-    calculadora.cadastraContribuicaoPrevidenciaria('Contribuição via carnê INSS', 130.0);
-    calculadora.cadastraContribuicaoPrevidenciaria('Contribuicao no contracheque IPREV', 500.3);
-
-    expect(calculadora.getTotalContribuicaoPrevidenciaria())
-      .toBeCloseTo(730.3, 2);
-    expect(calculadora.getContribuicaoPrevidenciaria())
-      .toEqual([
-        ['Contribuicao no contracheque', 100.0],
-        ['Contribuição via carnê INSS', 130.0],
-        ['Contribuicao no contracheque IPREV', 500.3]
-      ]);
-  });
 
   it.each<[[string, number][], number]>([
     [[['Contribuicao no contracheque', 430], ['Contribuição via carnê INSS', 490], ['Contribuicao no contracheque IPREV', 300]], 1220.0],
