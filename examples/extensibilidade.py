364fdd39f8322a3a2350c09b9adda2336b912459
Thiago Paiva <thiagohdaqw@gmail.com>
2023-02-08 20:32:04 -0300

    TP3 - add extensibility example good

--- examples/extensibilidade.py
+++ examples/extensibilidade.py
@@ -1,25 +1,42 @@
+from typing import Callable, Tuple
+
 
 class HttpRouter:
+	routes: dict[Tuple[str, str], Callable]
+	
+	def __init__(self):
+		self.routes = {}
+
+	def register_route(self, method: str, resource: str, callback: Callable):
+		self.routes[(method, resource)] = callback
+
 	def handle_http_request(self, http_request: str):
 		method, resource, *request = http_request.split(' ', 2)
+		
+		if (method, resource) in self.routes:
+			self.routes[(method, resource)](request)
 
-		if method == 'GET':
-			if resource == '/':
-				... # handle / GET request
-			elif resource == '/home':
-				... # handle /home GET Request
-			# ...
-		if method == 'POST':
-			if resource == '/api/login':
-				... # handle POST /api/login
-			elif resource == '/api/profile':
-				... # handle POST /api/profile
-		#...
+def handle_get_root(request):
+	...
 
+def handle_get_home(request):
+	...
 
+def handle_post_login(request):
+	...
+def handle_post_profile(request):
+	...
 
 http_router = HttpRouter()
 
+# Registering routes
+http_router.register_route('GET', '/', handle_get_root)
+http_router.register_route('GET', '/home', handle_get_home)
+http_router.register_route('POST', '/api/login', handle_post_login)
+http_router.register_route('GET', '/api/profile', handle_post_profile)
+
+
+# Handling requests
 http_router.handle_http_request("GET / HTTP/1.1\nHost localhost\n...")
 http_router.handle_http_request("GET /home HTTP/1.1\n...")
 http_router.handle_http_request("POST /api/login HTTP/1.1\n...")
